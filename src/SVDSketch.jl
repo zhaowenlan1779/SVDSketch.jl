fe1ba6ed83c6e095ff15ad6aad2ef78ec0777c9b
Pengfei Zhu <zhupf321@gmail.com>
2023-05-10 10:04:02 +0800

    Support complex matrices, use ElasticArrays

--- src/SVDSketch.jl
+++ src/SVDSketch.jl
@@ -2,29 +2,71 @@ module SVDSketch
 
 export svdsketch
 
-using LinearAlgebra: BlasReal, eigen, SVD, svd, tr
+using LinearAlgebra: BlasFloat, eigen, SVD, tr
+using ElasticArrays
 
-# function eigSVD(A)
-#     transposed = false
-#     if size(A, 1) < size(A, 2)
-#         A = A'
-#         transposed = true
-#     end
+function eigSVD(A, tol)
+    transposed = false
+    if size(A, 1) < size(A, 2)
+        A = A'
+        transposed = true
+    end
 
-#     B = A' * A;
-#     D, V = eigen(B)
-#     S = sqrt.(D)
+    B = A' * A;
+    D, V = eigen(B)
+    D = real(D)
 
-#     U = A * (V ./ S')
-#     if transposed
-#         return V, S, U
-#     else
-#         return U, S, V
-#     end
-# end
+    # Eliminate negative & very small eigenvalues
+    idx = searchsortedfirst(D, tol^2)
+    if idx > length(D)
+        error("SVDSketch: Could not find eigenvalue for eigSVD. Your `tol` may be set too high.")
+    end
 
-svdsketch(A::AbstractMatrix{<:BlasReal}, tol=eps(eltype(A))^(1/4); kwargs...) = _svdsketch(A, tol; kwargs...)
-svdsketch(A::AbstractMatrix{Complex{T}}, tol=missing; kwargs...) where T = throw(MethodError(svdsketch, Any[A,tol,kwargs...]))
+    D, V = D[idx:end], V[:, idx:end]
+    S = sqrt.(D)
+
+    U = A * (V ./ S')
+    if transposed
+        return V, S, U
+    else
+        return U, S, V
+    end
+end
+
+eps(T) = Base.eps(T)
+eps(::Type{Complex{T}}) where {T} = eps(T)
+
+@doc raw"""
+    svdsketch(A[, tol]; [maxrank, blocksize, maxiter, poweriter]) -> (U, S, Vt, apxerror)
+
+Returns the singular value decomposition (SVD) of a low-rank matrix sketch of ``A``.
+The matrix sketch only reflects the most important features of ``A`` (up to a tolerance),
+which enables faster calculation of the SVD of large matrices compared to using `svds`.
+
+The sketch of ``A`` satisfies that ``\|U \Sigma V^T - A\|_F / \|A\|_F \leq \text{tol}``.
+The default value for `tol` is `eps(eltype(A))^(1/4)`.
+
+In addition to the SVD, the vector `apxerror` is returned, whose entries represent the
+relative approximation error in each iteration, ``\|U \Sigma V^T - A\|_F / \|A\|_F``.
+The length of `apxerror` is equal to the number of iterations, and `apxerror[end]` is
+the relative approximation error of the output.
+
+# Options
+`maxrank`: The rank of the matrix sketch will not exceed `maxrank`.
+The default value is `minimum(size(A))`.
+
+`blocksize`: A larger value reduces the number of needed iterations, but might also
+result in the result having higher rank than necessary to achieve convergence.
+The default value is `min(max(floor(Integer, 0.1*size(A, 1)), 5), maxrank)`.
+
+`maxiter`: The maximum number of iterations for the algorithm.
+The default value is `maxrank ÷ blocksize`.
+
+`poweriter`: The number of power iterations performed within each iteration of the algorithm.
+Power iterations improve the orthogonality of the ``U`` and ``V`` outputs.
+The default value is 1.
+"""
+svdsketch(A::AbstractMatrix{<:BlasFloat}, tol=eps(eltype(A))^(1/4); kwargs...) = _svdsketch(A, tol; kwargs...)
 
 function svdsketch(A::AbstractMatrix{T}; kwargs...) where T
     Tnew = typeof(zero(T)/sqrt(one(T)))
@@ -49,13 +91,19 @@ function _svdsketch(A, tol;
 
     m, n = size(A)
     normA = sum(abs2, A)
-    threshold = tol^2 * normA
 
-    Z = similar(A, 0, 0)
-    Y = similar(A, m, 0)
-    W = similar(A, n, 0)
-    WTW = similar(A, 0, 0)
+    Z = Matrix{eltype(A)}(undef, 0, 0)
+    
+    Y = ElasticMatrix{eltype(A)}(undef, m, 0)
+    sizehint!(Y, m, 10 * blocksize)
+    
+    W = ElasticMatrix{eltype(A)}(undef, n, 0)
+    sizehint!(W, n, 10 * blocksize)
 
+    WTW = Matrix{eltype(A)}(undef, 0, 0)
+
+    apxerror = Vector{Float64}(undef, maxiter)
+    # oldblocksize = blocksize
     sizeB = 0
     for i = 1:maxiter
         w = randn(n, blocksize)
@@ -67,7 +115,7 @@ function _svdsketch(A, tol;
             else
                 w = A' * (A * w) - alpha * w
             end
-            w, ss, = svd(w)
+            w, ss, = eigSVD(w, tol)
             if j > 1 && ss[1] > alpha
                 alpha = (alpha + ss[1]) / 2
             end
@@ -85,39 +133,37 @@ function _svdsketch(A, tol;
             WTW = w' * w
         end
 
-        Y = [Y y]
-        W = [W w]
+        append!(Y, y)
+        append!(W, w)
         sizeB += blocksize
 
-        normB = tr(Z\WTW)
-        if normA - normB < threshold
+        apxerror[i] = sqrt(max(1 - real(tr(Z\WTW)) / normA, 0))
+        if apxerror[i] < tol || sizeB >= maxrank
+            apxerror = apxerror[1:i]
             break
         end
 
-        # TODO: Test out auto adjustment of block size, like what Matlab does
-        if sizeB >= maxrank
-            break
-        end
+        # Adjust block size
+        # if i > 1 && apxerror[i] > apxerror[i - 1] / 2
+        #     blocksize += oldblocksize
+        # end
+
         if sizeB + blocksize > maxrank
             blocksize = maxrank - sizeB
         end
     end
 
     D, V = eigen((Z + Z') / 2)
-
-    # Eliminate negative & very small eigenvalues
-    idx = searchsortedfirst(D, tol^2)
-    D, V = D[idx:end], V[:, idx:end]
-
     d = sqrt.(D)
     VS = V ./ d'
     C = VS' * WTW * VS
     C = (C + C') / 2 
     D2, V2 = eigen(C, sortby=λ->-abs(λ))
     d = sqrt.(D2)
-    Y = Y * (VS * V2)
-    W = W * (VS * (V2 ./ d'))
-    return SVD(Y, d, W')
+    VS *= V2
+    Y = Y * VS
+    W = W * VS ./ d'
+    return Y, d, W', apxerror
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,53 +1,72 @@
 using SVDSketch
 using Test, LinearAlgebra, SparseArrays, StableRNGs
 
-function testSVDMatch(S1, S2, r=size(S1.S, 1))
-    approxEq(a, b) = a ≈ b || a ≈ -b
+function testSVDMatch(r1, r2, rank=size(r1.S, 1))
+    approxEq(a, b) = sum(abs, a .* b) ≈ 1
 
-    @test S1.S[1:r] ≈ S2.S[1:r]
+    @test r1.S[1:rank] ≈ r2.S[1:rank]
     @testset "singular vectors" begin
-        for j = 1:r
-            @test approxEq(S1.U[:, j], S2.U[:, j])
-            @test approxEq(S1.V[:, j], S2.V[:, j])
+        for j = 1:rank
+            @test approxEq(r1.U[:, j], r2.U[:, j])
+            @test approxEq(r1.V[:, j], r2.V[:, j])
         end
     end
 end
 
 @testset "dense" begin
     rng = StableRNG(123)
-    A = rand(rng, 1:10, 10, 10) # Integer matrix, tests promotion as well
-    S1 = svdsketch(A)
-    S2 = svd(A)
 
-    testSVDMatch(S1, S2)
-    @test_throws ArgumentError svdsketch(A, blocksize=100)
+    @testset "real" begin
+        A = rand(rng, 1:10, 10, 10) # Integer matrix, tests promotion as well
+        U, S, Vt, = svdsketch(A)
+        r2 = svd(A)
+
+        testSVDMatch(SVD(U, S, Vt), r2)
+        @test_throws ArgumentError svdsketch(A, blocksize=100)
+    end
+
+    @testset "maxrank" begin
+        A = rand(rng, 1:10, 10, 10) # Integer matrix, tests promotion as well
+        U, S, = svdsketch(A, maxrank=3)
+
+        @test maximum(size(S)) <= 3
+    end
+
+    @testset "complex" begin
+        A = rand(rng, 1:10, 10, 10) + rand(rng, 1:10, 10, 10) * im
+        U, S, Vt, = svdsketch(A)
+        r2 = svd(A)
+
+        testSVDMatch(SVD(U, S, Vt), r2)
+        @test_throws ArgumentError svdsketch(A, blocksize=100)
+    end
 end
 
-@testset "maxrank" begin
-    rng = StableRNG(123)
-    A = rand(rng, 1:10, 10, 10) # Integer matrix, tests promotion as well
-    S1 = svdsketch(A, maxrank=3)
-
-    @test maximum(size(S1.S)) <= 3
-end
-
+# Following test cases are borrowed from Arpack.jl
 @testset "sparse" begin
-    A = sparse([1, 1, 2, 3, 4], [2, 1, 1, 3, 1], [2.0, -1.0, 6.1, 7.0, 1.5])
-    S1 = svdsketch(A)
-    S2 = svd(Array(A))
+    @testset "real" begin
+        A = sparse([1, 1, 2, 3, 4], [2, 1, 1, 3, 1], [2.0, -1.0, 6.1, 7.0, 1.5])
+        U, S, Vt, = svdsketch(A)
+        r2 = svd(Array(A))
 
-    testSVDMatch(S1, S2)
-    @test_throws ArgumentError svdsketch(A, blocksize=100)
-end
+        testSVDMatch(SVD(U, S, Vt), r2)
+        @test_throws ArgumentError svdsketch(A, blocksize=100)
+    end
 
-@testset "no complex" begin
-    # Dense
-    A = [1+1im 2+2im; 3+3im 4+4im]
-    @test_throws MethodError svdsketch(A)
+    @testset "maxrank" begin
+        A = sparse([1, 1, 2, 3, 4], [2, 1, 1, 3, 1], [2.0, -1.0, 6.1, 7.0, 1.5])
+        U, S, = svdsketch(A, maxrank=2)
 
-    # Sparse
-    A = sparse([1, 1, 2, 3, 4], [2, 1, 1, 3, 1], exp.(im*[2.0:2:10;]), 5, 4)
-    @test_throws MethodError svdsketch(A)
+        @test maximum(size(S)) <= 2
+    end
+
+    @testset "complex" begin
+        A = sparse([1, 1, 2, 3, 4], [2, 1, 1, 3, 1], exp.(im*[2.0:2:10;]), 5, 4)
+        U, S, Vt, = svdsketch(A, blocksize=1)
+        r2 = svd(Array(A))
+
+        testSVDMatch(SVD(U, S, Vt), r2)
+    end
 end
 
 @testset "low rank" begin
@@ -64,21 +83,30 @@ end
         A = U*Diagonal(S)*V'
 
         @testset "blocksize default" begin
-            F = svdsketch(A)
+            U, S, Vt, = svdsketch(A)
 
-            @test size(F.S, 1) == r
-            @test F.S[1:r] ≈ S
-            @test F.U'*F.U ≈ Matrix{Float64}(I, r, r)
-            @test F.V'*F.V ≈ Matrix{Float64}(I, r, r)
+            @test size(S, 1) == r
+            @test S[1:r] ≈ S
+            @test U'*U ≈ Matrix{Float64}(I, r, r)
+            @test Vt*Vt' ≈ Matrix{Float64}(I, r, r)
         end
 
-        @testset "blocksize bad" begin
-            F = svdsketch(A, blocksize=r-1)
+        @testset "blocksize r-1" begin
+            U, S, Vt, = svdsketch(A, blocksize=r-1)
 
-            @test size(F.S, 1) == r
-            @test F.S[1:r] ≈ S
-            @test F.U'*F.U ≈ Matrix{Float64}(I, r, r)
-            @test F.V'*F.V ≈ Matrix{Float64}(I, r, r)
+            @test size(S, 1) == r
+            @test S[1:r] ≈ S
+            @test U'*U ≈ Matrix{Float64}(I, r, r)
+            @test Vt*Vt' ≈ Matrix{Float64}(I, r, r)
+        end
+
+        @testset "blocksize r+1" begin
+            U, S, Vt, = svdsketch(A, blocksize=r+1)
+
+            @test size(S, 1) == r
+            @test S[1:r] ≈ S
+            @test U'*U ≈ Matrix{Float64}(I, r, r)
+            @test Vt*Vt' ≈ Matrix{Float64}(I, r, r)
         end
     end
 end
